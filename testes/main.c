68e08be9492e765776383a0d08317a4e824ddfd3
jefersonfg <jefersonfguimaraes@gmail.com>
2017-10-10 00:24:42 -0300

    Atualizado programa de teste para executar diversos cyield.

--- testes/main.c
+++ testes/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "../include/cthread.h"
 
+#include <unistd.h>
+
 void* TestFunc(void *arg) {
-    printf("Print da TestFunc!\n");
-    return;
+    printf("Print da TestFunc antes do primeiro cyield!\n");
+
+    //sleep(1);
+
+    cyield();
+
+    printf("Print da TestFunc depois do primeiro cyield!\n");
+
+    cyield();
+
+    printf("Print final da TestFunc!\n");
 }
 
 int main() {
@@ -14,6 +25,8 @@ int main() {
 
     printf("O valor de retorno da função ccreate é: %d\n", t1);
 
+    printf("Print da main antes do primeiro cyield!\n");
+
     cyield();
 
     printf("Print da main depois do primeiro cyield!\n");
@@ -21,6 +34,10 @@ int main() {
     cyield();
 
     printf("Print da main depois do segundo cyield!\n");
+
+    cyield();
+
+    printf("Print da main depois do terceiro cyield!\n");
 
     return 0;
 }
